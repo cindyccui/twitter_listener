b7af2dbf9066b700a731f77d0dcdfb58416052ab
Nick Malleson <nick@campus-10-41-57-161.wireless.leeds.ac.uk>
2014-07-16 10:38:29 +0100

    Completed function that recursively adds users to the graph. Hit rate limit problems. The code should detect these and sleep for 5 mins before continuing, but not tested thoroughly yet.

--- network_listner.py
+++ network_listner.py
@@ -2,6 +2,7 @@
 
 import sys
 import os
+import time # To allow for waiting to stop going over rate limits
 
 import networkx as nx # Library to work with graphs
 import matplotlib.pyplot as plt # G
@@ -10,6 +11,7 @@ import configparser # To parse the configuration file
 
 import tweepy
 from tweepy import OAuthHandler # For OAuthentication
+from tweepy import TweepError 
 
 
 # Global parameters
@@ -23,12 +25,18 @@ INITIAL = [ "nickmalleson"]
 #INITIAL = [ "nickmalleson", "frogo", "NikLomax", "ajheppenstall", "adam_dennett" ]
 
 
-def draw_graph(g):
+def draw_graph(g,  pos=None):
     """Draw the given graph using matplotlib. E.g. see:
     http://networkx.github.io/documentation/latest/examples/drawing/weighted_graph.html
-    http://networkx.github.io/documentation/networkx-1.9/reference/drawing.html"""
+    http://networkx.github.io/documentation/networkx-1.9/reference/drawing.html
+    
+    Parameters:
+        g - the graph to draw
+        pos - the layout to use (default is nx.spring_layout). For help, see nx.layout documentation
+    """
+    if pos == None:
+        pos = nx.spring_layout(g)
 
-    pos=nx.spring_layout(g) #positions for all nodes
     nx.draw_networkx_nodes(g, pos)
     nx.draw_networkx_edges(g, pos)
     nx.draw_networkx_labels(g, pos)
@@ -38,7 +46,7 @@ def draw_graph(g):
 
 # Method that recursively builds up a graph
 
-def build_graph(g, user, depth=0):
+def build_graph(g, user, parent, depth=0):
     """
     Recursively builds up a graph of followers.
     
@@ -46,9 +54,43 @@ def build_graph(g, user, depth=0):
 
     g - the networkx graph to build up
     user - the user to search from (assume a User tweepy object)
+    parent - the person who is friends with this user (could be None)
     depth - counts the number of followers so far so we know when to stop (default 0)
     """
-    XXXX HERE USE CODE BELOW TO IMPLEMENT THIS FUNCTION
+    
+    # First get the names of the users from the User objects (User provides a dict which allows
+    # access to all parameters
+    username = user.__dict__['screen_name']
+    parentname = ""
+    if parent!=None:
+        parentname = parent.__dict__['screen_name']
+
+    print "Analysing user ",username,"whose parent is",\
+            parentname if parent!=None else "none"
+
+    # Add nodes and edges to the graph
+    g.add_node(username, user=user)
+
+    if parent != None:
+        g.add_edge(username, parentname)
+
+    # Now do recurse over all the user's followers, unless we've reached the maximum depth
+    if depth > 0:
+        # To get followers requires a call to the Twitter API. Check and catch rate limitting
+        # problems
+        fail = True # Assume failure of api call and loop undil it doesn't fail
+        followers = None
+        while (fail):
+            try:
+                followers = user.followers()
+                fail = False # If get here then the api call succeeded
+            except TweepError as e:
+                print "Caught error:",str(e)
+                print "Assuming it is a rate limitting problem, waiting 5 mins"
+                time.sleep(300)# 5 mins = 300 seconds
+        for f in followers:
+            build_graph(g, f, user, depth-1)
+
 
 
 
@@ -82,21 +124,5 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth) # Note: help(api) shows the avaiable methods - very useful
 
 for name in INITIAL:
-    print "Getting user", name,
     user = api.get_user(name) # This returns a User object
-    # Turn this into a dictionary
-    print "Got",user.__dict__['screen_name']
-    # Add the name as the node and also include user object as an additional attribute
-    g.add_node(name, user=user) 
-    print "Followers:"
-    #followers = api.followers(user.id)
-    followers = user.followers()
-    for f in followers:
-        # Get information about each follower and add them to the graph
-        followername =f.__dict__['screen_name'] 
-        print "\tGot",followername
-        g.add_node(followername, user=f) # Again use the name as key and add additional attribute
-        g.add_edge(name, f)
-    print g
-    break
-    
+    build_graph(g, user=user, parent=None, depth=DEPTH)
