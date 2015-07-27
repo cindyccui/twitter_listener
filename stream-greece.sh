01b7422b68945f84db19283b1b3e14ad15378b30
Nick Malleson <n.malleson06@leeds.ac.uk>
2015-07-27 14:30:38 +0100

    Editted bounding region around Athens as all of Greece too large

--- stream-greece.sh
+++ stream-greece.sh
@@ -1,2 +1,7 @@
 # Starts the streaming program with Greece bounding box (southwest corner, then northeast)
-python streaming.py -l 19.37 34.80 28.25 47.78
+
+# All of Greece (too large, gets limitted):
+#python streaming.py -l 19.37 34.80 28.25 47.78
+
+# Just around Athens (all of Atiki region):
+python streaming.py -l 22.89 35.82 24.09 38.34
